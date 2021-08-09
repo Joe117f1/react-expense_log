65f26cf5455c42f9d72affdb2165214fc8e37a50
Yoav Hirshberg <yoav.yh@gmail.com>
2021-08-09 20:16:08 +0300

    reformatting

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,18 +3,20 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = props => {
-    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-    const totalMaximum = Math.max(...dataPointValues);
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
 
-    return <div className='chart'>
-        {props.dataPoints.map(dataPoint =>
-            <ChartBar
-                key={dataPoint.label}
-                value={dataPoint.value}
-                maxValue={totalMaximum}
-                label={dataPoint.label}
-            />)}
+  return (
+    <div className='chart'>
+      {props.dataPoints.map(dataPoint =>
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />)}
     </div>
+  );
 };
 
 export default Chart;
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,19 +3,21 @@ import './ExpensesList.css';
 
 const ExpensesList = (props) => {
 
-    if (props.items.length === 0) {
-        return <h2 className="expenses-list__fallback">No expenses found.</h2>;
-    };
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No expenses found.</h2>;
+  }
 
-    return <ul className="expenses-list">
-        {props.items.map(item =>
-        (<ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-        />))}
+  return (
+    <ul className="expenses-list">
+      {props.items.map(item =>
+      (<ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />))}
     </ul>
+  )
 };
 
 export default ExpensesList;
